8a41fda327cb8f4c1595d2a4ae1dc70032eeefd4
Buraczek <klimaszewska19@gmail.com>
2017-09-30 16:45:19 +0200

    wiecej-mniej button

--- js/app.js
+++ js/app.js
@@ -23,8 +23,33 @@ menu.style.display = 'none';
 	
 });
 
+//zad 2
+
+var buttons = document.querySelectorAll('.read-more');
 
 
+
+     // Poniższa linijka sprawdza, czy `style.display` jest ustawiony na `none` LUB jest pusty
+     // Dwie kreski obok siebie, czyli || oznaczają LUB, więc aby wykonał się kod podany w nawiasie klamrowym
+     // warunek po jednej albo po drugiej stronie kresek musi być spełniony
+
+function showHide() {
+
+	var textArea = this.previousElementSibling;
+	
+	if (textArea.style.display === 'none' || textArea.style.display === '') {
+		textArea.style.display = 'block';
+		this.innerHTML = 'MNIEJ <span class=glyphion-chevron-up></span>' ;
+	} else {
+		textArea.style.display = 'none' ;
+		this.innerHTML = 'WIĘCEJ <span class="glyphion-chevron-down"></span>' ;
+	}
+}
+
+for (var i=0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', showHide);
+	
+}
 
 
 
